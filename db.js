b266426a76a3cd04d9e8c4bb431606648fbd9227
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2022-12-08 09:05:11 +0100

    finsh

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+require('dotenv').config();
+const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 
-  // ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
